f8dbd5e26e35585876fb0b22c621ca7811fca7d1
Noor-laptob <noureldinelsherbiny5@gmail.com>
2021-05-16 03:43:45 +0200

    Testing + modifying search_Dfs function

--- Node.py
+++ Node.py
@@ -2,7 +2,7 @@ class Node():
 
     def __init__(self, name, p=None, goal=False):
         self.name = name
-        self.node = []
+        self.child_Node = []
         self.parent = p
         self.edges = {}
         self.goal=goal
@@ -10,7 +10,7 @@ class Node():
 
     # Getting the child by its number
     def get_By_Num(self,child):
-        return self.node[child]
+        return self.child_Node[child]
 
     # Getting the parent of the node
     def get_Parent(self):
@@ -18,14 +18,14 @@ class Node():
 
     # Getting the child by it's name
     def get_By_Name(self, data):
-        for child in range(0, len(self.node)):
-            if (self.node[child].name == data):
-                return self.node[child]
+        for child in range(0, len(self.child_Node)):
+            if (self.child_Node[child].name == data):
+                return self.child_Node[child]
 
     # Adding a new child to the Node
-    def add(self,name,weight):
-        node1 = Node(name=name)
-        self.node.append(node1)
+    def add(self,name,weight,goal=False):
+        node1 = Node(name=name,goal=goal)
+        self.child_Node.append(node1)
         node1.parent = self
         self.edges['{}'.format(name)]=weight
         return node1
@@ -34,20 +34,20 @@ class Node():
     def search_Dfs (self,start_node):
         visited = []
         fringe = [start_node]
+        solution = ''+start_node.name+' '
         while fringe is not None:
             x= len(fringe) - 1
             n=fringe[x]
             fringe.pop(x)
             if (n.goal):
                 p=n
-                solution=''
                 while (p.parent != None):
                     solution += p.name + ' '
                     p=p.parent
                 return solution
             else:
                 visited.append(n)
-                for c in n.child:
+                for c in n.child_Node:
                     found=False
                     for v in visited:
                         if v.name == c.name:
--- main.py
+++ main.py
@@ -4,7 +4,16 @@ x=Node(name='A')
 
 x.add(name='g', weight=50)
 x.add(name='b', weight=60)
+x.add(name='h',weight=80)
 
 z=x.get_By_Name('g')
+c=x.get_By_Name('h')
+z.add(name='j',weight=20)
+c.add(name='l',weight=30)
+s=c.get_By_Num(0)
+s.add(name='m',goal=True,weight=40)
+
+print(x.search_Dfs(x))
+
 print(z.parent.name)
 print(x.edges)
